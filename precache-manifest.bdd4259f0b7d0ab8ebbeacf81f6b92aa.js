5b96d5e155866953d227366f028844e1e4afdc24
Code Sandbox <hello@codesandbox.io>
2021-12-04 12:56:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.bdd4259f0b7d0ab8ebbeacf81f6b92aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b92e7aee88b853d78a83f482ea4af1",
+    "url": "https://kotisyamala.github.io/csb-vvy2q/index.html"
+  },
+  {
+    "revision": "af1b8592536a311f8aec",
+    "url": "https://kotisyamala.github.io/csb-vvy2q/static/css/main.1d57fb8d.chunk.css"
+  },
+  {
+    "revision": "db50462be6b800881004",
+    "url": "https://kotisyamala.github.io/csb-vvy2q/static/js/2.281c8f62.chunk.js"
+  },
+  {
+    "revision": "ed961b1d682fddd39f72e5d7dd46c4a2",
+    "url": "https://kotisyamala.github.io/csb-vvy2q/static/js/2.281c8f62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1b8592536a311f8aec",
+    "url": "https://kotisyamala.github.io/csb-vvy2q/static/js/main.702a7b55.chunk.js"
+  },
+  {
+    "revision": "f177d8504dcce199cab6",
+    "url": "https://kotisyamala.github.io/csb-vvy2q/static/js/runtime-main.648a83d4.js"
+  },
+  {
+    "revision": "e798b08f13bd9e0fe7c92ae26358e217",
+    "url": "https://kotisyamala.github.io/csb-vvy2q/static/media/bitcoin.e798b08f.png"
+  },
+  {
+    "revision": "d38c60827e3ea0dab148baaae2809f6d",
+    "url": "https://kotisyamala.github.io/csb-vvy2q/static/media/summary.d38c6082.png"
+  }
+]);
